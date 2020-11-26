31a4e70c6d4265b7a7148abbc0e516a870cb307e
Palumbo <fcojnavarrowork@gmail.com>
2020-11-26 18:41:55 +0100

    Añadidas todas las rutas get

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -20,33 +20,64 @@ function renderHome(req, res) {
     });
 }
 
-function renderChef(req, res) {
+function renderChef(req, res, next) {
     const { id } = req.params;
 
-    const chefsQuery = `SELECT * FROM ${chefsTable} WHERE id = ${id}`;
-    const platesQuery = `SELECT * FROM ${platesTable} WHERE chef_id = ${id}`;
+    if(isNaN(id)) {
+        next();
+    } 
+    
+    else {
+        const chefsQuery = `SELECT * FROM ${chefsTable} WHERE id = ${id}`;
+        const platesQuery = `SELECT * FROM ${platesTable} WHERE chef_id = ${id}`;
 
-    connection.query(chefsQuery, function(chefsError, chefsData) {
-        if(chefsError) {
-            throw chefsError;
-        }
+        connection.query(chefsQuery, function(chefsError, chefsData) {
+            if(chefsError) {
+                throw chefsError;
+            }
 
-        if(!chefsData.length) {
-            res.render('notfound', { error: `¡Ups! El chef al que intentas acceder no existe.` });
-        } else {
-            let [chef] = chefsData;
+            if(!chefsData.length) {
+                res.render('notfound', { error: `¡Ups! El chef al que intentas acceder no existe.` });
+            } else {
+                let [chef] = chefsData;
 
-            connection.query(platesQuery, function(platesError, platesData) {
-                if(platesError) {
-                    throw platesError;
-                }
+                connection.query(platesQuery, function(platesError, platesData) {
+                    if(platesError) {
+                        throw platesError;
+                    }
 
-                chef.plates = platesData;
-                console.log(chef);
-                res.render('chef', { chef });
-            });
-        }
-    });
+                    chef.plates = platesData;
+                    res.render('chef', { chef });
+                });
+            }
+        });
+    }
 }
 
-module.exports = { renderHome, renderChef };
+function renderPlatesForm(req, res) {
+    let plate = {};
+ 
+    if(!Object.values(req.params).length) {
+        res.render('forms/plates', { plate });
+    } else {
+        const { id } = req.params;
+        const platesQuery = `SELECT * FROM ${platesTable} WHERE id = ${id}`;
+
+        connection.query(platesQuery, function(error, data) {
+            if(error) {
+                throw error;
+            }
+
+            [plate] = data;
+            res.render('forms/plates', { plate });
+        });
+    }
+} 
+
+function renderNotFound(req, res) {
+    res.render('notfound', { error: `¡Ups! La dirección a la que intentas acceder no existe.` });
+}
+
+module.exports = { 
+    renderHome, renderChef, renderPlatesForm, renderNotFound 
+};
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 // const upload = require('../config/storage.js');
-const { renderHome, renderChef } = require('../controllers/indexController');
+const { 
+    renderHome, renderChef, renderPlatesForm, renderNotFound
+} = require('../controllers/indexController');
 
 /* GET home page. */
 router.get('/', renderHome);
 
+/* GET plate form page. */
+router.get('/plates', renderPlatesForm);
+router.get('/plates/:id', renderPlatesForm);
+
 /* GET chef page. */
 router.get('/:id', renderChef);
+
+
+/* GET Not Found page. */
+router.get('*', renderNotFound);
 
 module.exports = router;
